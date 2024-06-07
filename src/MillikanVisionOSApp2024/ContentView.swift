b6f2ecd6037888571d047fd410a954e1dbb46973
yankchina <yankchina@gmail.com>
2024-06-07 20:11:37 +0800

    Clean Reality Content

--- src/MillikanVisionOSApp2024/ContentView.swift
+++ src/MillikanVisionOSApp2024/ContentView.swift
@@ -12,15 +12,14 @@ import RealityKitContent
 struct ContentView: View {
     var body: some View {
         VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
+//            Model3D(named: "Scene", bundle: realityKitContentBundle)
+//                .padding(.bottom, 50)
             Text("Hello, world!")
         }
         .padding()
     }
 }
 
-#Preview(windowStyle: .automatic) {
-    ContentView()
-}
+//#Preview(windowStyle: .automatic) {
+//    ContentView()
+//}
